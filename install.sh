8ae61bc2ef03f8c6d546c0305fdef2f25a20edea
Qian Lei <qianlei90@gmail.com>
2017-01-06 19:05:57 +0800

    other: 支持pyenv的插件

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ mv $HOME/.gitconfig $HOME/.pip $HOME/.pyenv $HOME/.tmux $HOME/.tmux.conf $HOME/.
 ln -sf $f2tm_dir/git/gitconfig $HOME/.gitconfig
 ln -sf $f2tm_dir/python/pip $HOME/.pip
 ln -sf $f2tm_dir/python/pyenv $HOME/.pyenv
+ln -sf $f2tm_dir/python/pyenv-implict $HOME/.pyenv/plugins
+ln -sf $f2tm_dir/python/pyenv-virtualenv $HOME/.pyenv/plugins
 ln -sf $f2tm_dir/tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $f2tm_dir/tmux/tmux $HOME/.tmux
 ln -sf $f2tm_dir/vim/vimrc $HOME/.vimrc
