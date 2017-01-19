c022d9965611bf7819c7405cce61c42014275e67
Qian Lei <qianlei90@gmail.com>
2017-01-19 11:04:27 +0800

    other: 修正了install脚本

--- install.sh
+++ install.sh
@@ -2,16 +2,38 @@
 
 f2tm_dir="$HOME/Fly2TheMoon"
 backup_dir="$HOME/backup"
+config_dir="$HOME/.config"
 
 if [ ! -d $backup_dir ];
 then
     mkdir -p $backup_dir
 fi
+if [ ! -d $config_dir ];
+then
+    mkdir -p $config_dir
+fi
 
-mv $HOME/.gitconfig $HOME/.pip $HOME/.pyenv $HOME/.tmux $HOME/.tmux.conf $HOME/.vim $HOME/.vimrc $HOME/.zshrc $backup_dir/
+file="
+    $HOME/.gitconfig
+    $HOME/.pip
+    $HOME/.pyenv
+    $HOME/.tmux
+    $HOME/.tmux.conf
+    $HOME/.vim
+    $HOME/.vimrc
+    $HOME/.zshrc
+    $config_dir/pip
+"
+
+for _f in $file
+do
+    if [ -e $_f ]; then
+        mv $_f $backup_dir
+    fi
+done
 
 ln -sf $f2tm_dir/git/gitconfig $HOME/.gitconfig
-ln -sf $f2tm_dir/python/pip $HOME/.pip
+ln -sf $f2tm_dir/python/pip $config_dir/pip
 ln -sf $f2tm_dir/python/pyenv $HOME/.pyenv
 ln -sf $f2tm_dir/python/pyenv-implict $HOME/.pyenv/plugins
 ln -sf $f2tm_dir/python/pyenv-virtualenv $HOME/.pyenv/plugins
