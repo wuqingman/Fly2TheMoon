69b3bbe31f2e4ee397ee719c57789a414429c2dc
Qian Lei <qianlei90@gmail.com>
2017-02-26 22:26:29 +0800

    fix(zsh&other): 修正安装脚本，修正zsh的别名
    Signed-off-by: Qian Lei <qianlei90@gmail.com>

--- install.sh
+++ install.sh
@@ -36,7 +36,3 @@ ln -sf $f2tm_dir/tmux/tmux $HOME/.tmux
 ln -sf $f2tm_dir/vim/vimrc $HOME/.vimrc
 ln -sf $f2tm_dir/vim/vim $HOME/.vim
 ln -sf $f2tm_dir/zsh/zshrc $HOME/.zshrc
-
-if which docker > /dev/null 2>&1; then
-    curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f59c564e.m.daocloud.io
-fi
