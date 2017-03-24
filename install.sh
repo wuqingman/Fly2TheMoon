a8f4f33ceac106623a2925656843f83a0d46f135
Qian Lei <qianlei90@gmail.com>
2017-03-24 10:29:39 +0800

    fix(other): 修正安装脚本

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+# 配置变量
 f2tm_dir="$HOME/Fly2TheMoon"
 backup_dir="$HOME/backup"
+XDG_CONFIG_DIRS="$HOME/.config"
 
-if [ ! -d $backup_dir ];
-then
+# 创建目录
+if [ ! -d $backup_dir ]; then
     mkdir -p $backup_dir
 fi
+if [ ! -d $XDG_CONFIG_DIRS ]; then
+    mkdir -p $XDG_CONFIG_DIRS
+fi
 
+# 备份已有的配置文件
 file="
     $HOME/.gitconfig
     $HOME/.pip
@@ -17,15 +23,15 @@ file="
     $HOME/.zshrc
 "
 
-for _f in $file
-do
+for _f in $file; do
     if [ -e $_f ]; then
         mv $_f $backup_dir
     fi
 done
 
-ln -sf $f2tm_dir/git/gitconfig $HOME/.gitconfig
-ln -sf $f2tm_dir/python/pip $HOME/.pip
+# 安装现有的配置
+ln -sf $f2tm_dir/git $XDG_CONFIG_DIRS/
+ln -sf $f2tm_dir/python/pip $XDG_CONFIG_DIRS/
 ln -sf $f2tm_dir/tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $f2tm_dir/vim/vimrc $HOME/.vimrc
 ln -sf $f2tm_dir/vim/vim $HOME/.vim
